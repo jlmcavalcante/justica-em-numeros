4b936efa1cd3b00f0b7801a374b685770c391f27
José Cavalcante <joseluismoreiracavalcante@gmail.com>
2024-11-01 13:13:22 -0300

    Menu judicial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,8 @@ import { MainComponent } from './components/main/main.component';
 import { CircularMenu3Component } from './components/circular-menu-3/circular-menu-3.component';
 
 export const routes: Routes = [
-  { path: 'jud', component: MainComponent },
-  { path: 'adm', component: CircularMenu3Component },
-  { path: '', redirectTo: '/jud', pathMatch: 'full' },
-  { path: '**', redirectTo: '/jud' },
+  { path: 'administrativo', component: MainComponent },
+  { path: 'judicial', component: CircularMenu3Component },
+  { path: '', redirectTo: '/judicial', pathMatch: 'full' },
+  { path: '**', redirectTo: '/judicial' },
 ];
