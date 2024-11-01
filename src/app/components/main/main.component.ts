646147983e139f0fd8f85eb47b04e665a4b5fc13
José Cavalcante <joseluismoreiracavalcante@gmail.com>
2024-11-01 16:16:14 -0300

    fix menu 3

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -44,7 +44,6 @@ export class MainComponent implements OnInit {
 
   resetText() {
     this.currentTitle = 'Justiça em números';
-    this.currentText =
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id porta magna. Pellentesque congue elit quis mauris pulvinar, non dignissim leo porttitor. Praesent laoreet aliquam turpis eget porttitor. Suspendisse sodales sed tortor ac ornare. Duis massa turpis, vehicula sit amet sodales ac, congue id lectus. Etiam ut augue ac lacus efficitur accumsan eu at augue. Aenean quis sem nec eros aliquam lacinia at at libero. Sed a nibh a sapien eleifend porta. Nullam bibendum metus felis, non blandit tellus semper sit amet. In egestas dui vitae velit pulvinar accumsan. Integer condimentum, erat at efficitur consequat, enim nulla aliquet libero, vitae euismod elit velit in est. Quisque porttitor elementum ante sit amet tincidunt. ';
+    this.currentText = 'Desde 2004, o Relatório Justiça em Números é a principal fonte das estatísticas oficiais do Poder Judiciário, publicado anualmente, e revela a realidade dos tribunais brasileiros com profundidade, oferecendo um panorama detalhado sobre a estrutura, litigiosidade e indicadores essenciais para apoiar a Gestão Judiciária; a partir de 2022, passou a utilizar a Base Nacional de Dados do Poder Judiciário – DataJud, fonte primária para o Sistema de Estatística do Poder Judiciário – SIESPJ, o que trouxe mais transparência, aprimoramento na qualidade da informação, além de maior eficiência e racionalidade na coleta de dados processuais nos tribunais.'
   }
 }
---
+++ src/app/data/menu-juicial-data.ts
@@ -0,0 +1,17 @@
+export const menuJudicialData = [
+  {
+    title: 'Metas e Indicadores',
+    description:
+      'Este painel apresenta informações essenciais sobre as Metas e Indicadores do Tribunal de Justiça do Estado do Piauí, permitindo que a alta administração monitore o desempenho do sistema judiciário. Os principais indicadores incluem a taxa de congestionamento, os índices de produtividade de servidores e magistrados, e os tempos médios de julgamento e baixas. A taxa de congestionamento reflete a proporção de processos pendentes em relação ao total em tramitação, oferecendo uma visão clara da eficiência do fluxo processual. Os índices de produtividade destacam a performance dos servidores e magistrados, enquanto os tempos médios de julgamento e baixas avaliam a celeridade na resolução de casos. O índice de atendimento à demanda mostra a capacidade do tribunal de equilibrar o ingresso e a saída de processos. Além disso, o painel apresenta o cumprimento das metas nacionais do CNJ, evidenciando o compromisso do TJPI em alinhar suas práticas com as diretrizes para a melhoria da qualidade da justiça. Com dados organizados e atualizados, este painel é uma ferramenta fundamental para promover a transparência e a excelência na prestação de serviços judiciais.',
+  },
+  {
+    title: 'Gestão Processual',
+    description:
+      'Este painel fornece informações essenciais para as unidades judiciárias do Tribunal de Justiça do Estado do Piauí, permitindo um acompanhamento eficaz da tramitação dos processos. Os dados incluem o número de processos conclusos e em secretaria, além da comparação entre processos tramitando, suspensos e arquivados. Os indicadores também apresentam a relação entre processos julgados e pendentes, facilitando a identificação de áreas que precisam de atenção. Ademais, o painel oferece uma visão temporal dos casos novos em relação aos baixados, proporcionando insights sobre a dinâmica do fluxo processual. Outra seção relevante do painel é a distribuição dos processos nas tarefas do PJe (Processo Judicial Eletrônico), que ajuda as unidades a visualizar como os processos estão organizados em diferentes etapas do sistema.',
+  },
+  {
+    title: 'Gestão de Pessoas',
+    description:
+      'Este painel é uma ferramenta essencial para as unidades judiciárias do Tribunal de Justiça do Estado do Piauí, focando na análise e melhoria da gestão de pessoas. Ele apresenta informações sobre o índice de produtividade dos servidores e magistrados, permitindo uma avaliação do desempenho individual e coletivo. Além disso, o painel detalha as atividades judiciais e administrativas realizadas pelos servidores, oferecendo uma visão abrangente das funções desempenhadas em cada unidade. Os dados sobre afastamentos também são incluídos, proporcionando insights sobre a disponibilidade da equipe e seu impacto nas operações diárias.',
+  }
+];
