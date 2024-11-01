b319a3e45aced9b9848a60555b78a95c4deec0d8
José Cavalcante <joseluismoreiracavalcante@gmail.com>
2024-11-01 14:56:30 -0300

    Add new text to each menu

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component, HostListener, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { menuAdministrativoData } from '../../data/menu-administrativo-data';
 
 interface ItemText {
   title: string;
@@ -13,55 +14,13 @@ interface ItemText {
   standalone: true,
   imports: [MatIconModule, MatButtonModule, CommonModule],
   templateUrl: './main.component.html',
-  styleUrl: './main.component.scss'
+  styleUrl: './main.component.scss',
 })
-export class MainComponent implements OnInit{
+export class MainComponent implements OnInit {
   public isSmallScreen: boolean = false;
   currentTitle: string = 'Justiça em números';
-  currentText: string =
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id porta magna. Pellentesque congue elit quis mauris pulvinar, non dignissim leo porttitor. Praesent laoreet aliquam turpis eget porttitor. Suspendisse sodales sed tortor ac ornare. Duis massa turpis, vehicula sit amet sodales ac, congue id lectus. Etiam ut augue ac lacus efficitur accumsan eu at augue. Aenean quis sem nec eros aliquam lacinia at at libero. Sed a nibh a sapien eleifend porta. Nullam bibendum metus felis, non blandit tellus semper sit amet. In egestas dui vitae velit pulvinar accumsan. Integer condimentum, erat at efficitur consequat, enim nulla aliquet libero, vitae euismod elit velit in est. Quisque porttitor elementum ante sit amet tincidunt. ';
-  itemDescriptions: ItemText[] = [
-    {
-      title: 'Estrutura de Pessoal',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet convallis neque. Pellentesque nulla magna, feugiat at tempus suscipit, porttitor ut dolor. Donec eget nisl ut diam placerat egestas. Duis rutrum ipsum ex, in elementum nibh imperdiet vitae. Aliquam a sodales nunc, at egestas leo. Ut pharetra laoreet aliquet. Phasellus sit amet neque ut arcu aliquam pellentesque.',
-    },
-    {
-      title: 'Produtividade',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet convallis neque. Pellentesque nulla magna, feugiat at tempus suscipit, porttitor ut dolor. Donec eget nisl ut diam placerat egestas. Duis rutrum ipsum ex, in elementum nibh imperdiet vitae. Aliquam a sodales nunc, at egestas leo. Ut pharetra laoreet aliquet. Phasellus sit amet neque ut arcu aliquam pellentesque.',
-    },
-    {
-      title: 'Prêmios de Qualidade',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet convallis neque. Pellentesque nulla magna, feugiat at tempus suscipit, porttitor ut dolor. Donec eget nisl ut diam placerat egestas. Duis rutrum ipsum ex, in elementum nibh imperdiet vitae. Aliquam a sodales nunc, at egestas leo. Ut pharetra laoreet aliquet. Phasellus sit amet neque ut arcu aliquam pellentesque.',
-    },
-    {
-      title: 'Orçamentos',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet convallis neque. Pellentesque nulla magna, feugiat at tempus suscipit, porttitor ut dolor. Donec eget nisl ut diam placerat egestas. Duis rutrum ipsum ex, in elementum nibh imperdiet vitae. Aliquam a sodales nunc, at egestas leo. Ut pharetra laoreet aliquet. Phasellus sit amet neque ut arcu aliquam pellentesque.',
-    },
-    {
-      title: 'Patrimônio',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet convallis neque. Pellentesque nulla magna, feugiat at tempus suscipit, porttitor ut dolor. Donec eget nisl ut diam placerat egestas. Duis rutrum ipsum ex, in elementum nibh imperdiet vitae. Aliquam a sodales nunc, at egestas leo. Ut pharetra laoreet aliquet. Phasellus sit amet neque ut arcu aliquam pellentesque.',
-    },
-    {
-      title: 'Plano de Gestão',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet convallis neque. Pellentesque nulla magna, feugiat at tempus suscipit, porttitor ut dolor. Donec eget nisl ut diam placerat egestas. Duis rutrum ipsum ex, in elementum nibh imperdiet vitae. Aliquam a sodales nunc, at egestas leo. Ut pharetra laoreet aliquet. Phasellus sit amet neque ut arcu aliquam pellentesque.',
-    },
-    {
-      title: 'Contratos e Licitações',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet convallis neque. Pellentesque nulla magna, feugiat at tempus suscipit, porttitor ut dolor. Donec eget nisl ut diam placerat egestas. Duis rutrum ipsum ex, in elementum nibh imperdiet vitae. Aliquam a sodales nunc, at egestas leo. Ut pharetra laoreet aliquet. Phasellus sit amet neque ut arcu aliquam pellentesque.',
-    },
-    {
-      title: 'Obras',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet convallis neque. Pellentesque nulla magna, feugiat at tempus suscipit, porttitor ut dolor. Donec eget nisl ut diam placerat egestas. Duis rutrum ipsum ex, in elementum nibh imperdiet vitae. Aliquam a sodales nunc, at egestas leo. Ut pharetra laoreet aliquet. Phasellus sit amet neque ut arcu aliquam pellentesque.',
-    },
-  ];
+  currentText: string = 'Desde 2004, o Relatório Justiça em Números é a principal fonte das estatísticas oficiais do Poder Judiciário, publicado anualmente, e revela a realidade dos tribunais brasileiros com profundidade, oferecendo um panorama detalhado sobre a estrutura, litigiosidade e indicadores essenciais para apoiar a Gestão Judiciária; a partir de 2022, passou a utilizar a Base Nacional de Dados do Poder Judiciário – DataJud, fonte primária para o Sistema de Estatística do Poder Judiciário – SIESPJ, o que trouxe mais transparência, aprimoramento na qualidade da informação, além de maior eficiência e racionalidade na coleta de dados processuais nos tribunais.'
+  itemDescriptions: ItemText[] = menuAdministrativoData;
 
   constructor() {}
 
---
+++ src/app/data/menu-administrativo-data.ts
@@ -0,0 +1,42 @@
+export const menuAdministrativoData = [
+  {
+    title: 'Estrutura de Pessoal',
+    description:
+      'Este painel oferece uma visão completa e consolidada da estrutura de pessoal do Tribunal de Justiça do Estado, incluindo o quantitativo de servidores, magistrados, terceirizados, cedidos, auxiliares de justiça, estagiários e aposentados. Abaixo estão os números de cargos e funções comissionadas, distribuídos conforme a lotação, com detalhes de ocupação e preenchimento por área. Também são apresentadas as informações das comarcas e unidades judiciais, classificadas conforme a natureza, abrangendo a totalidade das comarcas do estado e as unidades especializadas, em conformidade com as diretrizes de lotação paradigma.',
+  },
+  {
+    title: 'Produtividade',
+    description:
+      'Este painel oferece uma análise dos principais indicadores processuais e de desempenho do Tribunal de Justiça, apresentando dados consolidados sobre o acervo processual, casos novos, processos baixados e julgados. Esses dados são essenciais para o monitoramento contínuo da atividade judiciária, permitindo uma visão detalhada do fluxo de processos em todas as unidades judiciais. Além dos indicadores de movimentação processual, o painel exibe a relação entre o número de processos baixados e o de casos novos, definido como Índice de Atendimento à Demanda e os Índices de Produtividade de magistrados e servidores, possibilitando uma avaliação precisa da performance individual e coletiva das equipes judiciárias. Por fim, o painel também acompanha o cumprimento das Metas Nacionais do Conselho Nacional de Justiça (CNJ), oferecendo uma visão clara do comprometimento com os objetivos estabelecidos para o aprimoramento da prestação jurisdicional.',
+  },
+  {
+    title: 'Prêmios de Qualidade',
+    description:
+      'Este painel destaca os dois principais prêmios de excelência no Judiciário: o Prêmio Melhores Práticas do Tribunal de Justiça do Estado do Piauí e o Prêmio CNJ de Qualidade. Ambos incentivam a inovação, a eficiência e o compromisso dos servidores e magistrados com a melhoria contínua do atendimento ao cidadão. O Prêmio Melhores Práticas do TJPI reconhece ideias criativas e soluções tecnológicas propostas para modernizar o tribunal e aprimorar o atendimento, valorizando a proatividade de quem busca melhorias além das atividades diárias. O Prêmio CNJ de Qualidade, concedido pelo Conselho Nacional de Justiça, avalia o desempenho dos tribunais em Governança, Produtividade, Transparência e Tecnologia, reconhecendo aqueles que atendem às metas nacionais e promovem uma gestão estratégica e eficiente. Este painel reúne dados atualizados sobre as premiações e reforça o compromisso do TJPI com a excelência no serviço público.',
+  },
+  {
+    title: 'Orçamento',
+    description:
+      'Este painel apresenta uma visão abrangente das informações orçamentárias do Tribunal de Justiça do Estado do Piauí, destacando os principais dados necessários para uma análise estratégica pela alta administração. Nele, estão consolidadas informações sobre o orçamento total, valores executados e disponíveis, permitindo uma avaliação precisa do status financeiro atual e o monitoramento contínuo da execução orçamentária. O painel detalha as despesas em áreas prioritárias, incluindo investimentos em obras, gastos com a folha de pagamento, despesas com serviços terceirizados, tecnologia da informação e o limite de gastos com despesas de pessoal. Esses dados permitem uma análise completa e facilitam a identificação de ajustes necessários para que o TJPI mantenha a eficiência financeira e o cumprimento de metas institucionais.',
+  },
+  {
+    title: 'Patrimônio',
+    description:
+      'Este painel é uma ferramenta estratégica destinada à alta administração do Tribunal de Justiça do Estado do Piauí, focando na gestão do patrimônio e dos materiais do Poder Judiciário. Ele apresenta os principais indicadores utilizados para calcular o Índice de Desenvolvimento Sustentável (IDS) do Judiciário, possibilitando uma análise abrangente da sustentabilidade nas operações. Os indicadores incluídos no painel abrangem aspectos como consumo de recursos naturais, eficiência energética, gestão de resíduos e utilização de materiais sustentáveis. Essas métricas são fundamentais para avaliar o impacto ambiental das atividades do Judiciário e para identificar oportunidades de melhoria na gestão de recursos. Além disso, o painel fornece uma visão geral sobre a conservação e manutenção do patrimônio, permitindo que a alta administração identifique áreas que necessitam de atenção e implemente estratégias para otimizar o uso dos ativos.',
+  },
+  {
+    title: 'Plano de Gestão',
+    description:
+      'Este painel oferece uma visão clara e detalhada do acompanhamento do Plano de Gestão do Tribunal de Justiça do Estado do Piauí, permitindo que a alta administração monitore o progresso das metas estabelecidas. Cada item do plano é apresentado com uma indicação do nível de cumprimento, fornecendo uma avaliação precisa do desempenho em relação aos objetivos propostos. Para cada meta, o painel destaca os prazos estabelecidos e a situação atual, permitindo uma análise rápida das áreas em que o Tribunal está avançando e aquelas que podem exigir atenção especial. Essa abordagem facilita a identificação de possíveis desvios e a implementação de ações corretivas para garantir que as metas sejam atingidas dentro dos prazos estipulados.',
+  },
+  {
+    title: 'Contratos e Licitações',
+    description:
+      'Este painel apresenta uma visão abrangente sobre os processos de licitação e os contratos do Tribunal de Justiça do Estado do Piauí, permitindo um acompanhamento detalhado e eficiente pela alta administração. O painel inclui informações sobre os procedimentos de contratação, atas vigentes, contratos celebrados, contratos que estão próximos a vencer, além de acordos de cooperação e convênios firmados. Para cada categoria, são fornecidos dados específicos, como o objeto da contratação, o valor, a duração do contrato e a situação atual. As atas vigentes detalham as condições acordadas, enquanto os contratos próximos a vencer destacam a necessidade de gestão proativa para garantir a continuidade dos serviços e o atendimento às demandas do Tribunal. Além disso, o painel abrange os acordos de cooperação e convênios, enfatizando a colaboração com outras entidades e a busca por parcerias que ampliem a capacidade de atuação do TJPI.',
+  },
+  {
+    title: 'Obras',
+    description:
+      'Este painel reúne informações detalhadas sobre as obras do Tribunal de Justiça do Estado do Piauí, facilitando o acompanhamento e a supervisão pela alta administração. São apresentados dados das obras já concluídas, das que estão em andamento e das que se encontram em processo de licitação. Para cada obra, o painel fornece uma descrição completa, incluindo o objetivo do projeto, prazo previsto para conclusão, valor investido e o estágio atual. As obras concluídas refletem os avanços já alcançados na infraestrutura do Tribunal, enquanto as obras em andamento e em licitação demonstram o compromisso com o crescimento contínuo e a modernização das instalações. Atualizado regularmente, o painel garante transparência e eficiência no uso dos recursos públicos, além de fortalecer a estrutura do TJPI, assegurando que as necessidades do sistema de justiça sejam atendidas com qualidade e responsabilidade.',
+  },
+];
