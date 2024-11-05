574699f3fa32f59e0e6eeee83484e5aca3631286
José Cavalcante <joseluismoreiracavalcante@gmail.com>
2024-11-05 15:02:10 -0300

    Fix judicial cicle menu

--- src/app/components/circular-menu-3/circular-menu-3.component.ts
+++ src/app/components/circular-menu-3/circular-menu-3.component.ts
@@ -12,6 +12,7 @@ import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { CookieService } from 'ngx-cookie-service';
 
 interface ItemText {
   title: string;
@@ -49,7 +50,7 @@ export class CircularMenu3Component implements OnInit {
   selectedInstancia: string | null = null;
   selectedOrgao: string | null = null;
 
-  constructor(private orgaoJulgadorService: OrgaoJulgadorService) {}
+  constructor(private orgaoJulgadorService: OrgaoJulgadorService, private cookieService: CookieService) {}
 
   ngOnInit() {
     this.checkScreenSize();
@@ -67,6 +68,8 @@ export class CircularMenu3Component implements OnInit {
         console.log('Requisição completada com sucesso');
       },
     });
+
+    this.loadSelections();
   }
 
   private extractUniqueInstancias(): void {
@@ -128,5 +131,37 @@ export class CircularMenu3Component implements OnInit {
     this.currentTitle = 'Justiça em números';
     this.currentText =
       'Desde 2004, o Relatório Justiça em Números é a principal fonte das estatísticas oficiais do Poder Judiciário, publicado anualmente, e revela a realidade dos tribunais brasileiros com profundidade, oferecendo um panorama detalhado sobre a estrutura, litigiosidade e indicadores essenciais para apoiar a Gestão Judiciária; a partir de 2022, passou a utilizar a Base Nacional de Dados do Poder Judiciário – DataJud, fonte primária para o Sistema de Estatística do Poder Judiciário – SIESPJ, o que trouxe mais transparência, aprimoramento na qualidade da informação, além de maior eficiência e racionalidade na coleta de dados processuais nos tribunais.';
+  }
+
+  saveSelections() {
+    const expirationDays = 7;
+    const expirationDate = new Date();
+    expirationDate.setDate(expirationDate.getDate() + expirationDays);
+
+    const selections = {
+      instancia: this.selectedInstancia,
+      orgao: this.selectedOrgao
+    };
+
+    // Converte para string JSON e salva no cookie
+    this.cookieService.set('userSelections', JSON.stringify(selections), expirationDate, '/', '', true, 'Lax');
+    console.log('Seleções salvas no cookie:', selections);
+  }
+
+  loadSelections() {
+    const cookie = this.cookieService.get('userSelections');
+    if (cookie) {
+      const selections = JSON.parse(cookie);
+      this.selectedInstancia = selections.instancia;
+      this.selectedOrgao = selections.orgao;
+      console.log('Seleções carregadas do cookie:', selections);
+    }
+  }
+
+  clearSelections() {
+    this.selectedInstancia = null;
+    this.selectedOrgao = '';
+    this.cookieService.delete('userSelections');
+    console.log('Seleções e cookie apagados');
   }
 }
